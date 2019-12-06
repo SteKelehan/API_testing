8c93989e2443bf05cd1d7a5f5e3cc5529a89575d
Ste <stephenkelehan@gmail.com>
2019-12-06 20:52:19 +0000

    Simple http api

--- NODEHTTP/server.js
+++ NODEHTTP/server.js
@@ -22,15 +22,41 @@ const server = http.createServer((req, res) => {
         body.push(chunk);
     }).on('end', () => {
         body = Buffer.concat(body).toString();
-        console.log(body);
-    })
 
-    res.end(
-        JSON.stringify({
-            success: true,
-            data: todos
-        
-    }));
+        let status = 404;
+        const response = {
+            success: false,
+            data: null
+        };
+
+        if(method === 'GET' && url === '/todos'){
+            status = 200;
+            response.success = true;
+            response.data = todos;
+        }else if (method === 'POST' && url == '/todos'){
+            // Have to send it through JSON.parse for it to be an object
+            const {id, text} = JSON.parse(body);
+
+            if(!id || !text){
+                staus = 400;
+                response.data = null;
+            }else{
+                todos.push({id, text});
+                status = 201;
+                response.sucess = true;
+                response.data = todos;
+            }
+
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'X-powered-By' : 'Node.js'
+        })
+
+        res.end(JSON.stringify(response));
+    });
+
 });
 
 const PORT = 5000;
